d2c4bc2b28cdae41630684f0c345917e454c807b
Mohamed Ragaey <mohamed.ragaey@toptal.com>
2025-04-15 06:17:55 +0200

    refactor: update ThemeToggle design

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../store/store';
 import { toggleTheme } from '../features/theme/themeSlice';
+import { RootState } from '../store/store';
 
 const ThemeToggle = () => {
   const dispatch = useDispatch();
@@ -12,12 +12,20 @@ const ThemeToggle = () => {
   };
 
   return (
-    <button
-      onClick={handleThemeToggle}
-      className="mb-4 p-2 bg-blue-500 text-white rounded"
-    >
-      Toggle Theme ({theme === 'light' ? 'Light' : 'Dark'} Mode)
-    </button>
+    <div className="flex items-center mb-4">
+      <span className="mr-2 text-sm text-gray-600 dark:text-gray-300">
+        {theme === 'light' ? 'Light Mode' : 'Dark Mode'}
+      </span>
+      <div
+        onClick={handleThemeToggle}
+        className="relative inline-block w-12 h-6 bg-gray-300 dark:bg-gray-700 rounded-full cursor-pointer"
+      >
+        <span
+          className={`absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform duration-300 ease-in-out transform ${theme === 'light' ? 'translate-x-0' : 'translate-x-6'
+            }`}
+        />
+      </div>
+    </div>
   );
 };
 
