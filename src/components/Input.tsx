81b293ecc8ae72c3cda0aec2fcd0db16173fefcc
Mohamed Ragaey <mohamed.ragaey@toptal.com>
2025-04-15 06:39:37 +0200

    chore: update input component to have hint

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import ErrorMessage from "@/components/ErrorMessage";
+import HintMessage from "@/components/HintMessage";
 import { FieldValues, Path, UseFormRegister } from "react-hook-form";
 
 type InputProps<TFieldValues extends FieldValues> = {
@@ -8,12 +9,13 @@ type InputProps<TFieldValues extends FieldValues> = {
     type: string;
     register: UseFormRegister<TFieldValues>;
     error?: string;
+    hint?: string;
 }
 
 const Input = <TFieldValues extends FieldValues>(props: InputProps<TFieldValues>) => {
 
     return (
-        <>
+        <div>
             <div className="relative z-0">
                 <input id={props.id}
                     type={props.type}
@@ -21,8 +23,9 @@ const Input = <TFieldValues extends FieldValues>(props: InputProps<TFieldValues>
                     className={`${props.error ? 'border-red-600 dark:border-red-500 dark:focus:border-red-500 focus:border-red-600' : ''} block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`} placeholder=" " />
                 <label htmlFor={props.id} className={`${props.error ? 'text-red-600 dark:text-red-500' : ''} absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}>{props.label}</label>
             </div>
+            {!!props.hint && <HintMessage message={props.hint} />}
             {!!props.error && <ErrorMessage message={props.error} />}
-        </>
+        </div>
     );
 };
 
